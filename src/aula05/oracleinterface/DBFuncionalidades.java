457dfc325ceb27766060d2f5c557b9e02156a6ab
ICMC <ICMC>
2018-10-02 15:27:21 -0300

    First commit

---
+++ src/aula05/oracleinterface/DBFuncionalidades.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula05.oracleinterface;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author junio
+ */
+public class DBFuncionalidades {
+    Connection connection;
+    Statement stmt;
+    ResultSet rs;
+    JTextArea jtAreaDeStatus;
+    
+    public DBFuncionalidades(JTextArea jtaTextArea){
+        jtAreaDeStatus = jtaTextArea;
+    }
+    
+    public boolean conectar(){       
+        try {
+            DriverManager.registerDriver (new oracle.jdbc.OracleDriver());
+            connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@192.168.183.15:1521:orcl",
+                    "L8937483",
+                    "Furukawa*Nagisa18");
+            return true;
+        } catch(SQLException ex){
+            jtAreaDeStatus.setText("Problema: verifique seu usuário e senha");
+        }
+        return false;
+    }
+    public void pegarNomesDeTabelas(JComboBox jc){
+        String s = "";
+        try {
+            s = "SELECT table_name FROM user_tables";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(s);
+            while (rs.next()) {
+                jc.addItem(rs.getString("table_name"));
+            }
+            stmt.close();
+        } catch (SQLException ex) {
+            jtAreaDeStatus.setText("Erro na consulta: \"" + s + "\"");
+        }        
+    }
+    public void exibeDados(JTable tATable, String sTableName){
+        /*Aqui preencho a tabela com os dados*/
+    }
+    //public void preencheComboBoxComRestricoesDeCheck
+    //public void preencheComboBoxComValoresReferenciados
+    //
+}
